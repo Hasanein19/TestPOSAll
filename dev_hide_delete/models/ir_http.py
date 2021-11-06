92c4aeeeeb21b6cb7383f6d0eef80dc1578089fb
Hasanein19 <developerhasanein@gmail.com>
2021-11-06 19:18:50 +0300

    Hasdelete

---
+++ dev_hide_delete/models/ir_http.py
@@ -0,0 +1,17 @@
+
+
+from odoo import models
+
+
+class Http(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    def session_info(self):
+        result = super(Http, self).session_info()
+        result.update({
+            'is_show_delete_button': True if (self.env.user.has_group(
+                'dev_hide_delete.group_display_delete_action')) else False
+        })
+        return result
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
