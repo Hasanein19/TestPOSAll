667368e51884edd39c88c77a3463c506d8151764
Hasanein19 <developerhasanein@gmail.com>
2021-11-06 19:14:06 +0300

    dl

--- Hasanain_hide_delete-14.0.1.0/dev_hide_delete/__manifest__.py
+++ Hasanain_hide_delete-14.0.1.0/dev_hide_delete/__manifest__.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2015 DevIntelle Consulting Service Pvt.Ltd (<http://www.devintellecs.com>).
-#
-#    For Module Support : devintelle@gmail.com  or Skype : devintelle 
+
+#    For Module Support : developerhasanein@gmail.com
 #
 ##############################################################################
 
@@ -45,12 +42,12 @@ odoo appp Hide delete button from action. hide delete, delete buttton hide, hide
     'auto_install': False,
     
     # author and support Details =============#
-    'author': 'DevIntelle Consulting Service Pvt.Ltd',
-    'website': 'http://www.devintellecs.com',    
-    'maintainer': 'DevIntelle Consulting Service Pvt.Ltd', 
-    'support': 'devintelle@gmail.com',
-    'price':9.0,
-    'currency':'EUR',
+    'author': 'Hasanein Abusadah',
+    'website': 'http://www.https://mt-sys.online/',
+    'maintainer': 'MTS',
+    'support': 'developerhasanein@gmail.com',
+    'price':50.0,
+    'currency':'SAR',
     #'live_test_url':'https://youtu.be/A5kEBboAh_k',
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
